b4db39b7a7a15d957e957403b40bdbcc0e52a39b
gutovesco <guto.vesco@hotmail.com>
2020-01-21 18:28:13 -0300

    functionalities, style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <h1>Sup</h1>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if(newItem.text!==""){
+      const items =[...this.state.items, newItem];
+      this.setState({
+        items:newItem,
+        currentItem:{
+          text:'',
+          key:''
+        }
+      })
+    }
+
+  }
+  render(){
+    return (
+      <div className="App">
+      <header> 
+        <form id="to-do-form" onSubmit={this.addItem}>
+          <input value={this.state.currentItem.text} onChange={this.handleInput} type="text" placeholder="Enter Text"></input>
+          <button type="submit">Add</button>
+        </form>
+      </header>
+      </div>
+    );
+  }
 }
 
 export default App;
